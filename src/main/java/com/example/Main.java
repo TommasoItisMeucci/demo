5c1e4403e2518279b6e3102590484de8f888de16
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:09:12 +0200

    prove con thread

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,17 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        int l = 200;
+        //Thread
+        MioThread t1 = new MioThread(l);//creazione 
+        MioThread t2 = new MioThread(l);
+        new Thread(t1).start();//avvio
+        new Thread(t2).start();
+       
+        
+
         System.out.println("Hello world!");
+
+
     }
 }
